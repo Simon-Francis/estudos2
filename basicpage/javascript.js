f697170c32cc9704bd9ca8f99cafda939f7cb323
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-09-24 11:39:50 -0300

    ;

---
+++ basicpage/javascript.js
@@ -0,0 +1,27 @@
+async function pesquisar() {
+    let cep = document.getElementById('cep').value
+    let result = document.getElementById('result')
+
+    if (cep.length == 0) {
+
+        console.log(cep, document.getElementById('cep'))
+        alert("Digite um cep primeiro")
+
+    } else {
+        
+    console.log(cep, document.getElementById('cep'))
+
+    let res = await fetch("https://brasilapi.com.br/api/cep/v2/"+cep)
+
+    let x = Object.values(await res.json())
+
+    console.log(x)
+    
+    for (let pos in x) {
+        result.innerHTML += `${x[pos]}<br>`    
+    }
+    }
+
+    
+}
+
